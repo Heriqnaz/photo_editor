73d6f9436ddef780b200b465de307eb69b4d9f61
Anna-Davoyan <67603904+Anna-Davoyan@users.noreply.github.com>
2020-07-29 09:00:27 +0400

    add subMenu and sideBar (#7)
    * add eslint configs for react
    
    * create sub menu, and sidebar

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,21 +1,49 @@
 import React from 'react';
 import '../css/content.css'
-import {Nav, Navbar} from 'react-bootstrap';
+import {Nav, Navbar, NavDropdown} from 'react-bootstrap';
 
 export function Content() {
+    const handleClick = () => {
+        const event = new CustomEvent('hideSidebar');
+        dispatchEvent(event);
+    };
     return (
-        <div className='content'>
+        <div className='content' onClick={handleClick}>
             <Navbar bg="light" variant="light" expand="lg">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#effects">Effects</Nav.Link>
-                    <Nav.Link href="#filter">Filter</Nav.Link>
-                    <Nav.Link href="#sticker">Sticker</Nav.Link>
-                    <Nav.Link href="#Crop">Crop</Nav.Link>
-                </Nav>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <NavDropdown title="Effects">
+                            <NavDropdown.Item href="#effects">Blur</NavDropdown.Item>
+                            <NavDropdown.Item href="#filter">Magic</NavDropdown.Item>
+                            <NavDropdown.Item href="#sticker">Pop Art</NavDropdown.Item>
+                            <NavDropdown.Item href="#effects">Blur</NavDropdown.Item>
+                            <NavDropdown.Item href="#filter">Magic</NavDropdown.Item>
+                            <NavDropdown.Item href="#sticker">Pop Art</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Frame">
+                            <NavDropdown.Item href="#effects">Birthday</NavDropdown.Item>
+                            <NavDropdown.Item href="#filter">Love</NavDropdown.Item>
+                            <NavDropdown.Item href="#sticker">Party Time</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Sticker">
+                            <NavDropdown.Item href="#effects">Flower</NavDropdown.Item>
+                            <NavDropdown.Item href="#filter">Butterfly</NavDropdown.Item>
+                            <NavDropdown.Item href="#sticker">Tree</NavDropdown.Item>
+                            <NavDropdown.Item href="#effects">Flower</NavDropdown.Item>
+                            <NavDropdown.Item href="#filter">Butterfly</NavDropdown.Item>
+                            <NavDropdown.Item href="#sticker">Tree</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#Crop">Crop</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
             <div className="img-box">
-                <div className='edit-img'></div>
+                <div className='edit-img'/>
             </div>
         </div>
     )
 }
+
+
+
