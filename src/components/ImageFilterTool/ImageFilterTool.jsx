2bd26e1a270d037760eb5b5fe313f68cc8af1842
sargsyanvahe <47384884+sargsyanvahe@users.noreply.github.com>
2020-08-02 02:54:08 +0400

    added redux tool state

--- src/components/ImageFilterTool/ImageFilterTool.jsx
+++ src/components/ImageFilterTool/ImageFilterTool.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import PropTypes from 'prop-types';
+import {connect} from "react-redux";
 import {Col, Container, Row} from 'react-bootstrap';
 import './ImageFilterTool.css'
 
