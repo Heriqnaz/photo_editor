96ecac0a65a7e74285ecfff801b08928b2a88194
sargsyanvahe <47384884+sargsyanvahe@users.noreply.github.com>
2020-08-04 12:05:45 +0400

    added build

---
+++ docs/precache-manifest.5f8ede4012bc0a6afb80da9b9103c66e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028c7992df89a21dbfbb4d03f3aa9843",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81c015dd37fcd8dad1a1",
+    "url": "/static/css/main.cb4f12d9.chunk.css"
+  },
+  {
+    "revision": "170b3a7b904c33c0e694",
+    "url": "/static/js/2.f6379fed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f6379fed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81c015dd37fcd8dad1a1",
+    "url": "/static/js/main.aa0b9015.chunk.js"
+  },
+  {
+    "revision": "2e67c52514ac2fe9a00c",
+    "url": "/static/js/runtime-main.a17792d7.js"
+  },
+  {
+    "revision": "29edbd0fd1dea104f298a670fb85b6f4",
+    "url": "/static/media/download-solid.29edbd0f.svg"
+  },
+  {
+    "revision": "5f2cc10b3a529f2eaae74660a8da8ee7",
+    "url": "/static/media/redo-alt-solid.5f2cc10b.svg"
+  },
+  {
+    "revision": "c6a0effbe867db0941bd5592f8f43810",
+    "url": "/static/media/search-solid.c6a0effb.svg"
+  },
+  {
+    "revision": "a75a5f095bf3a1155c66d012b647ca4c",
+    "url": "/static/media/tail-spin.a75a5f09.svg"
+  },
+  {
+    "revision": "4c10b4a7ae7345b9f6c49eed6a697563",
+    "url": "/static/media/undo-alt-solid.4c10b4a7.svg"
+  },
+  {
+    "revision": "2643a50b1c9bf7dea9470a8d25441599",
+    "url": "/static/media/upload-solid.2643a50b.svg"
+  }
+]);
